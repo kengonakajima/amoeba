465a3f2c6a6c09aa4207a1f63f4473724fd941aa
Kengo Nakajima <kengo.nakajima@gmail.com>
2015-08-19 13:13:07 +0900

    refactor

---
+++ Util.h
@@ -0,0 +1,46 @@
+// copied from cumino.h
+
+inline float maxf( float a, float b ){
+    return (a>b) ? a:b;
+}
+inline float maxf( float a, float b, float c ) {
+    return maxf( maxf(a,b), c );
+}
+inline float maxf( float a, float b, float c, float d ) {
+    return maxf( maxf(a,b), maxf(c,d) );
+}
+inline double maxd( double a, double b ){
+    return  (a>b) ? a:b;
+}
+inline double mind( double a, double b ){
+    return (a<b) ? a:b;
+}
+inline int maxi( int a, int b ){
+    return  (a>b) ? a:b;
+}
+inline int mini( int a, int b ){
+    return (a<b) ? a:b;
+}
+inline long random() { 
+	int top = rand();
+	int bottom = rand();
+	long out = ( top << 16 ) | bottom;
+	return out;
+}
+
+inline double range( double a, double b ) {
+    long r = random();
+    double rate = (double)r / (double)(0x7fffffff);
+    double _a = mind(a,b);
+    double _b = maxd(a,b);
+    return _a + (_b-_a)*rate;
+}
+inline int irange( int a, int b ) {
+    double r = range(a,b);
+    return (int)r;
+}
+
+
+void print( const char *fmt, ... );
+
+
